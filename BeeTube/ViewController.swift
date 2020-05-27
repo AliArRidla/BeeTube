389939c343a7571199264ab8e2909c7772c6c020
zero0dayali <officialali35@gmail.com>
2020-05-27 08:00:13 +0700

    Initial Commit

---
+++ BeeTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeeTube
+//
+//  Created by Ali on 27/05/20.
+//  Copyright © 2020 Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
